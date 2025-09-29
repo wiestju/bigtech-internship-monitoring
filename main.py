efbc8f23e4da91a1f0f1975ad6bac28a5c2fae1d
b267a <b267a@protonmail.com>
2025-09-29 18:16:04 +0200

    Added some print statements in the main.py

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from jobs.facebook import getJobsFacebook
 
 def main():
 
+    print("Getting all Jobs from Amazon...")
     getJobsAmazon()
+    print("Getting all Jobs from Microsoft...")
     getJobsMicrosoft()
+    print("Getting all Jobs from Facebook...")
     getJobsFacebook()
+    print("Updating Job Storage File...")
     utils.job_storage.update_job_storage()
+    print("Task ran successfully.")
 
 if __name__ == "__main__":
     main()
